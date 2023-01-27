9588d64a86e88f6addca3ebadf8fc747df9333a6
Comelpho <comelpho@gmail.com>
2023-01-27 08:03:54 +0300

    Доделал модели, расширил доступы к полям, дописал фильтры пользователей

--- src/api/second-table/controllers/second-table.js
+++ src/api/second-table/controllers/second-table.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::second-table.second-table');
+module.exports = createCoreController('api::second-table.second-table', {
+    async find (ctx) {
+        const user = ctx.state.user;
+
+        ctx.query.filters = {
+            ...(ctx.query.filters || {}),
+            owner: user.id
+        };
+
+        return super.find(ctx);
+    }
+});
--- src/api/third-table/controllers/third-table.js
+++ src/api/third-table/controllers/third-table.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::third-table.third-table');
+module.exports = createCoreController('api::third-table.third-table', {
+    async find (ctx) {
+        const user = ctx.state.user;
+
+        ctx.query.filters = {
+            ...(ctx.query.filters || {}),
+            owner: user.id
+        };
+
+        return super.find(ctx);
+    }
+});
